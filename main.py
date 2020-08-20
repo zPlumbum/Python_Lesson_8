ebd584a192c4cd2ce54d8a5977c673f3a8e8e311
Alexandr Kikot <academic77@yandex.ru>
2020-08-20 14:10:48 +0300

    Solution improved

--- main.py
+++ main.py
@@ -1,50 +1,43 @@
+from collections import Counter
+
+def print_top_10(words_list, file_type):
+    top_10 = Counter(words_list).most_common(10)
+    print(f'Топ-10 самых часто встречающихся слов в новостях ({file_type}):')
+    for place in top_10:
+        print(f'{place[0]} -> {place[1]}')
+
+def add_words_to_list(list_of_items, list_to_add):
+    for word in list_of_items:
+        if len(word) > 6:
+            list_to_add.append(word)
+
+
 # Задание №1
 import json
 
 all_words_json = []
-top_words_json = {}
 
 with open('newsafr.json', encoding='utf-8') as f:
     json_data = json.load(f)
 
 for item in json_data['rss']['channel']['items']:
     text = item['description'].split()
-    for word in text:
-        if len(word) > 6:
-            all_words_json.append(word)
-for word in all_words_json:
-    if word not in top_words_json.keys():
-        top_words_json.setdefault(word, all_words_json.count(word))
-
-def by_value(item):
-    return item[1]
-
-print('Топ-10 самых часто встречающихся слов в новостях (json):')
-for key, value in sorted(top_words_json.items(), key=by_value, reverse=True)[:10]:
-    print(key, '->', value)
+    add_words_to_list(text, all_words_json)
+print_top_10(all_words_json, 'json')
 print()
 
 # Задание №2
 import xml.etree.ElementTree as ET
 
 all_words_xml = []
-top_words_xml = {}
 
 parser = ET.XMLParser(encoding='utf-8')
 tree = ET.parse('newsafr.xml', parser)
 root = tree.getroot()
 
-news_list = root.findall('channel/item')
+news_list = root.findall('channel/item/description')
 
 for news in news_list:
-    text = news.find('description').text.split()
-    for word in text:
-        if len(word) > 6:
-            all_words_xml.append(word)
-for word in all_words_xml:
-    if word not in top_words_xml.keys():
-        top_words_xml.setdefault(word, all_words_xml.count(word))
-
-print('Топ-10 самых часто встречающихся слов в новостях (xml):')
-for key, value in sorted(top_words_xml.items(), key=by_value, reverse=True)[:10]:
-    print(key, '->', value)
+    text = news.text.split()
+    add_words_to_list(text, all_words_xml)
+print_top_10(all_words_xml, 'xml')
